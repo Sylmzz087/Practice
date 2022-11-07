8de0c4db2207c2b5e40174f20782599be1b09472
Sylmzz087 <Sylmzz087@gmail.com>
2022-11-07 15:50:49 +0100

    hello

--- src/main/java/day12_localdatetimeVarargsOverloadingOverriding/C04_Varargs.java
+++ src/main/java/day12_localdatetimeVarargsOverloadingOverriding/C04_Varargs.java
@@ -1,7 +1,9 @@
 package day12_localdatetimeVarargsOverloadingOverriding;
 
 public class C04_Varargs {
-
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
 
 
 }
